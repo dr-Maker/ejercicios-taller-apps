9121ebb6f92fd492d6499ee7ec58fcbcfd8c5a6b
Dream Maker <jonathanvenegas.dm@gmail.com>
2020-01-19 15:42:49 -0300

    algunas correcciones de decoracion y estilos

--- Layout/layout.js
+++ Layout/layout.js
@@ -1,34 +1,32 @@
-
-
-var seleccion= document.querySelector('#contDinamic');
-var lisHome= document.querySelector('#home');
-var lisZpty= document.querySelector('#zpty');
-var lisWz= document.querySelector('#wz');
+var seleccion = document.querySelector('#contDinamic');
+var lisHome = document.querySelector('#home');
+var lisZpty = document.querySelector('#zpty');
+var lisWz = document.querySelector('#wz');
 
 
 lisZpty.addEventListener("click", zpotify);
-function zpotify(){
-    seleccion.setAttribute("src","../zpotify/index.html");
+
+function zpotify() {
+    seleccion.setAttribute("src", "../zpotify/index.html");
     let text1 = document.querySelector('#namePag');
-    text1.innerHTML="Zpotify";
+    text1.innerHTML = "Zpotify";
 };
 
 
 
 lisWz.addEventListener("click", whazApp);
 
-function whazApp(){
-    seleccion.setAttribute("src","../WhazApp/index.html");
+function whazApp() {
+    seleccion.setAttribute("src", "../WhazApp/index.html");
     let text2 = document.querySelector('#namePag');
-    text2.innerHTML="whazApp";
-   };
+    text2.innerHTML = "WhazApp";
+};
 
-   
+
 lisHome.addEventListener("click", home);
-function home(){
-    seleccion.setAttribute("src","../Home/index.html");
+
+function home() {
+    seleccion.setAttribute("src", "../Home/index.html");
     let text3 = document.querySelector('#namePag');
-    text3.innerHTML="Home";
+    text3.innerHTML = "Home";
 }
-
-
--- WhazApp/Js/chat.js
+++ WhazApp/Js/chat.js
@@ -7,34 +7,36 @@ var user5 = document.querySelector("#boxFive");
 
 user1.addEventListener("click", acction1);
 
-function acction1(){
+function acction1() {
     var text1 = document.querySelector("#selctName");
-    text1.innerHTML="JUAN PEREZ PEREZ";
+    text1.innerHTML = "JUAN PEREZ PEREZ";
 }
 
 user2.addEventListener("click", acction2);
 
-function acction2(){
+function acction2() {
     var text2 = document.querySelector("#selctName");
-    text2.innerHTML="PERICO CASANOVA";
+    text2.innerHTML = "PERICO CASANOVA";
 }
 
 
 user3.addEventListener("click", acction3);
 
-function acction3(){
+function acction3() {
     var text3 = document.querySelector("#selctName");
-    text3.innerHTML="MIRANDA UNDURRAGA";
+    text3.innerHTML = "MIRANDA UNDURRAGA";
 }
 
 user4.addEventListener("click", acction4);
-function acction4(){
+
+function acction4() {
     var text4 = document.querySelector("#selctName");
-    text4.innerHTML="PAOLA ASENJO";
+    text4.innerHTML = "PAOLA ASENJO";
 }
 
 user5.addEventListener("click", acction5);
-function acction5(){
+
+function acction5() {
     var text5 = document.querySelector("#selctName");
-    text5.innerHTML="MIRIAM HERNANDEZ";
+    text5.innerHTML = "MIRIAM HERNANDEZ";
 }
